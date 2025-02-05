6c2af2a3c485250620bac4c4bc1ebf3a668b8935
Kabir5296 <manb5296@gmail.com>
2025-02-05 14:16:08 +0600

    distillloss

--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .model_utils import Adapter, AttentionBlock, MLP, AdaptedTimesformerBlock
+from .model_utils import Adapter, TimesFormerBlock
 from dataclasses import dataclass
 from typing import Optional
 
@@ -10,45 +10,90 @@ class CSTAOutput:
     logits: torch.FloatTensor = None
     predictions: torch.FloatTensor = None
     ce_loss: Optional[torch.FloatTensor] = None
+    distil_loss: Optional[torch.FloatTensor] = None
+    last_hidden_state: Optional[torch.FloatTensor] = None
 
 class CSTA(nn.Module):
-    def __init__(self, num_frames, img_size, patch_size, dim, num_classes, num_layers=12, num_channels = 3):
+    def __init__(self, 
+                 num_frames, 
+                 img_size, 
+                 patch_size, 
+                 dim, 
+                 num_classes, 
+                 num_layers=12, 
+                 num_channels = 3,
+                 num_heads = 8,
+                 init_with_adapters = True,
+                 calculate_distil_loss = False,
+                 ):
         super().__init__()
         self.dim = dim
         self.img_size = img_size
+        self.calculate_distil_loss = calculate_distil_loss
 
         # process video to patches and add positional embeddings, class token
         self.num_patches = (img_size // patch_size) ** 2
         self.patch_embed = nn.Conv2d(num_channels, dim, kernel_size=patch_size, stride=patch_size)
         self.pos_embed = nn.Parameter(torch.randn(self.num_patches * num_frames + 1, dim))
         self.cls_token = nn.Parameter(torch.zeros(1, 1, dim))
+        self.norm = nn.LayerNorm(dim)
 
         # keeping a list of adapters. Each transformer block has a list of adapters
         self.temporal_adapters = nn.ModuleList([nn.ModuleList() for _ in range(num_layers)])
         self.spatial_adapters = nn.ModuleList([nn.ModuleList() for _ in range(num_layers)])
 
         # keeping a list of transformer blocks
-        self.blocks = nn.ModuleList([AdaptedTimesformerBlock(dim) for _ in range(num_layers)])
+        self.blocks = nn.ModuleList([TimesFormerBlock(dim = dim, num_heads=num_heads) for _ in range(num_layers)])
 
         # keeping a list of classifiers
         self.classifiers = nn.ModuleList([nn.Linear(dim, num_classes)])
 
-        # tracking current task for use
-        self.current_task = 0
+        # add the first adapter to all the timesformer blocks
+        if init_with_adapters:
+            self.add_one_adapter_per_block()
 
-    def add_new_task_components(self, num_new_classes):
+        # keep the numbers stored somewhere
+        self.model_attributes = self.get_numbers()
+
+    def get_numbers(self):
+        total_blocks = len(self.blocks)
+        total_temporal_adapters = total_spatial_adapters = 0
+
+        for block_idx in range(len(self.blocks)):
+            total_temporal_adapters += len(self.temporal_adapters[block_idx])
+            total_spatial_adapters += len(self.spatial_adapters[block_idx])
+        adapters_per_block = int(total_spatial_adapters/len(self.blocks))
+        total_classifiers = len(self.classifiers)
+        current_task = adapters_per_block - 1
+        return {
+            "total_blocks": total_blocks,
+            "adapters_per_block": adapters_per_block,
+            "current_task": current_task,
+            "total_temporal_adapters": total_temporal_adapters,
+            "total_spatial_adapters": total_spatial_adapters,
+            "total_classifiers": total_classifiers,
+            "total_adapters": total_temporal_adapters + total_spatial_adapters,
+            "total_params": sum(p.numel() for p in self.parameters()),
+        }
+
+    def add_one_adapter_per_block(self):
+        for block_idx in range(len(self.blocks)):
+            self.temporal_adapters[block_idx].append(Adapter(self.dim))
+            self.spatial_adapters[block_idx].append(Adapter(self.dim))
+        self.model_attributes = self.get_numbers()
+
+    def add_one_new_classifier(self, num_new_classes):
         new_classifier = nn.Linear(self.classifiers[-1].in_features, num_new_classes)
         self.classifiers.append(new_classifier)
-        
-        dim = self.pos_embed.shape[-1]
-        for block_idx in range(len(self.blocks)):
-            self.temporal_adapters[block_idx].append(Adapter(dim))
-            self.spatial_adapters[block_idx].append(Adapter(dim))
+        self.model_attributes = self.get_numbers()
 
-        self.current_task += 1
-        self.freeze_old_components()
+    def add_new_task_components(self, num_new_classes):
+        self.add_one_adapter_per_block()
+        self.add_one_new_classifier(num_new_classes)
+        self.calculate_distil_loss = True
+        self.model_attributes = self.get_numbers()
 
-    def freeze_old_components(self):
+    def freeze_all_but_last(self):
         for classifier in self.classifiers[:-1]:
             for param in classifier.parameters():
                 param.requires_grad = False
@@ -58,13 +103,16 @@ class CSTA(nn.Module):
                 param.requires_grad = False
 
         for block_idx in range(len(self.blocks)):
-            for task_idx in range(self.current_task - 1):
-                for param in self.temporal_adapters[block_idx][task_idx].parameters():
+            for adapter in self.temporal_adapters[block_idx][:-1]:
+                for param in adapter.parameters():
                     param.requires_grad = False
 
-            for task_idx in range(self.current_task - 1):
-                for param in self.spatial_adapters[block_idx][task_idx].parameters():
+            for adapter in self.spatial_adapters[block_idx][:-1]:
+                for param in adapter.parameters():
                     param.requires_grad = False
+
+    def get_distil_loss(self, old_logits, new_logits):
+        return F.kl_div(F.log_softmax(new_logits, dim=1), F.softmax(old_logits, dim=1), reduction='batchmean')
 
     def forward(self, x, targets=None):
         B, T, C, H, W = x.shape
@@ -82,32 +130,74 @@ class CSTA(nn.Module):
         cls_tokens = self.cls_token.expand(B, -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
 
+        temporal_features = []
+        spatial_features = []
+
+        ce_loss = distil_loss = lt_loss = ls_loss = None
+        x_old = x
         for block_idx, block in enumerate(self.blocks):
-            temp_attn_output, _ = block.temporal_msa.msa(x, x, x)
-            for task_adapter in self.temporal_adapters[block_idx]:
-                temp_attn_output = temp_attn_output + task_adapter(temp_attn_output)
-            x = block.temporal_msa.norm(x + temp_attn_output)
-            
-            spatial_attn_output, _ = block.spatial_msa.msa(x, x, x)
-            for task_adapter in self.spatial_adapters[block_idx]:
-                spatial_attn_output = spatial_attn_output + task_adapter(spatial_attn_output)
-            x = block.spatial_msa.norm(x + spatial_attn_output)
-            x = block.norm(x + block.mlp(x))
+            # x goes to t_msa.
+            # t_msa output goes to all adapters (stored in temporal adapter features list)
+            # the final temporal output is the sum of all temporal adapter outputs, plus the t_msa and the input normalized
+            block_t_msa, _ = block.temporal_msa(x,x,x)
+            temporal_adapter_features = []
+            for temporal_adapters in self.temporal_adapters[block_idx]:
+                temporal_adapter_features.append(temporal_adapters(block_t_msa))
+            x = self.norm(x + block_t_msa + sum(temporal_adapter_features))
+            temporal_features.append(block_t_msa + sum(temporal_adapter_features))
+
+            # same as before but for spatial msa and adapters
+            block_s_msa, _ = block.spatial_msa(x,x,x)
+            spatial_adapter_features = []
+            for spatial_adapters in self.spatial_adapters[block_idx]:
+                spatial_adapter_features.append(spatial_adapters(block_s_msa))
+            x = self.norm(x + block_s_msa + sum(spatial_adapter_features))
+            spatial_features.append(block_s_msa + sum(spatial_adapter_features))
+
+            # final MLP
+            x = self.norm(block.mlp(x) + x)
+
+            if (self.training or self.calculate_distil_loss) and targets is not None:
+                block_t_msa_old, _ = block.temporal_msa(x_old,x_old,x_old)
+                temporal_adapter_features_old = []
+                for temporal_adapters in self.temporal_adapters[block_idx][:-1]:
+                    temporal_adapter_features_old.append(temporal_adapters(block_t_msa_old))
+                x_old = self.norm(x_old + block_t_msa_old + sum(temporal_adapter_features_old))
+
+                block_s_msa_old, _ = block.spatial_msa(x_old,x_old,x_old)
+                spatial_adapter_features_old = []
+                for spatial_adapters in self.spatial_adapters[block_idx][:-1]:
+                    spatial_adapter_features_old.append(spatial_adapters(block_s_msa_old))
+                x_old = self.norm(x_old + block_s_msa_old + sum(spatial_adapter_features_old))
+
+                x_old = self.norm(block.mlp(x_old) + x_old)
+
+        temporal_features = torch.stack(temporal_features, dim=1)
+        spatial_features = torch.stack(spatial_features, dim=1)
 
         x = x[:, 0]
+        x_old = x_old[:, 0]
         outputs = []
+        outputs_old = []
+
         for classifier in self.classifiers:
             outputs.append(classifier(x))
+            if (self.training or self.calculate_distil_loss) and targets is not None:
+                outputs_old.append(classifier(x_old))
 
         final_logits = torch.cat(outputs, dim=1)
+        predictions = torch.softmax(final_logits, dim=1)
+
         # cross entropy loss
-        ce_loss = None
         if targets is not None:
             ce_loss = F.cross_entropy(final_logits, targets)
-        predictions = torch.softmax(final_logits, dim=1)
+            if (self.training or self.calculate_distil_loss) and targets is not None:
+                distil_loss = self.get_distil_loss(torch.cat(outputs_old, dim=1), final_logits)
 
         return CSTAOutput(
             logits = final_logits,
             ce_loss = ce_loss,
+            distil_loss = distil_loss,
             predictions = predictions,
+            last_hidden_state = x,
         )
--- src/model_utils.py
+++ src/model_utils.py
@@ -11,46 +11,37 @@ class Adapter(nn.Module):
 
     def forward(self, x):
         return self.fc_up(self.gelu(self.fc_down(x)))
-
-class AttentionBlock(nn.Module):
-    def __init__(self, dim, num_heads=8):
+    
+class TimesFormerBlock(nn.Module):
+    def __init__(self, dim, num_heads=8, factor=4):
         super().__init__()
-        self.msa = nn.MultiheadAttention(dim, num_heads)
-        self.adapter = Adapter(dim)
-        self.norm = nn.LayerNorm(dim)
+        """
+        x_temporal = T_MSA(x)
+        x = NORM(x_temporal + x)
 
-    def forward(self, x):
-        attn_output, _ = self.msa(x, x, x)
-        adapted = self.adapter(attn_output)
-        return self.norm(x + adapted + attn_output)
+        x_spatial = S_MSA(x)
+        x = NORM(x + x_spatial)
 
-class MLP(nn.Module):
-    def __init__(self, dim, factor=4):
-        super().__init__()
+        x = NORM(MLP(x) + x)
+        """
+
+        self.temporal_msa = nn.MultiheadAttention(dim, num_heads)
+        self.spatial_msa = nn.MultiheadAttention(dim, num_heads)
+        
         self.mlp = nn.Sequential(
             nn.Linear(dim, dim * factor),
             nn.GELU(),
             nn.Linear(dim * factor, dim)
         )
+        
         self.norm = nn.LayerNorm(dim)
 
     def forward(self, x):
-        return self.norm(x + self.mlp(x))
+        temporal_attention, _ = self.temporal_msa(x,x,x)
+        x = self.norm(x + temporal_attention)
 
-class AdaptedTimesformerBlock(nn.Module):
-    def __init__(self, dim, num_heads=8, factor=4):
-        super().__init__()
-        self.temporal_msa = AttentionBlock(dim, num_heads)
-        self.spatial_msa = AttentionBlock(dim, num_heads)
-        self.mlp = nn.Sequential(
-            nn.Linear(dim, dim * 4),
-            nn.GELU(),
-            nn.Linear(dim * 4, dim)
-        )
-        self.norm = nn.LayerNorm(dim)
+        spatial_attention, _ = self.spatial_msa(x,x,x)
+        x = self.norm(x + spatial_attention)
 
-    def forward(self, x):
-        x = self.temporal_msa(x)
-        x = self.spatial_msa(x)
         x = self.norm(self.mlp(x) + x)
         return x
